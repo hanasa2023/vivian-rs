e8da73f43926e026b207e89855a001a4dd9eb577
hanasaki <hanasakayui2022@gmail.com>
2025-05-24 09:46:51 +0000

    ✨ Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Rust development environment with Nix Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs =
+              [ eza openssl pkg-config rust-bin.beta.latest.default ];
+
+            shellHook = ''
+              alias ls=eza
+            '';
+          };
+      });
+}
